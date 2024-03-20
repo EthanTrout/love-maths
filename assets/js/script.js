17c62d6dca2d0690ab0619d2ea88252a7d0889d9
EthanTrout <ethan-james-trout@outlook.com>
2024-03-20 15:19:32 +0000

    feat: displaying addtion question to user

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,18 +4,32 @@ document.addEventListener("DOMContentLoaded",function(){
     for(let button of buttons){
         button.addEventListener("click",function(){
             if(this.getAttribute("data-type") === "submit"){
-                alert("you clicked submit");
+                alert("You clicked submit")
             }
             else{
                 let gameType = this.getAttribute("data-type")
-                alert(`you clicked ${gameType}`);
+                runGame(gameType)
             }
         })
     }
+    runGame("addition")
 })
 
-function runGame(){
+function runGame(gameType){
     //creates two random numbers
     let num1 = Math.floor(Math.random() * 25) +1;
     let num2 = Math.floor(Math.random() * 25) +1;
+
+    if(gameType === "addition"){
+        displayAdditionQuestion(num1,num2);
+    }
+    else{
+        alert(`Unkown game type ${gameType}`)
+    }
+}
+
+function displayAdditionQuestion(operand1,operand2){
+ document.getElementById("operand1").textContent = operand1;
+ document.getElementById("operand2").textContent = operand2;
+ document.getElementById("operator").textContent = "+";
 }
