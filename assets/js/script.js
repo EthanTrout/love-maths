b45f33c1ea7c40e408155c2ea7652b513896267d
EthanTrout <ethan-james-trout@outlook.com>
2024-03-20 14:00:06 +0000

    feat:added event listeners for load and button clicks

---
+++ assets/js/script.js
@@ -0,0 +1,15 @@
+document.addEventListener("DOMContentLoaded",function(){
+    let buttons = document.getElementsByTagName("button");
+
+    for(let button of buttons){
+        button.addEventListener("click",function(){
+            if(this.getAttribute("data-type") === "submit"){
+                alert("you clicked submit");
+            }
+            else{
+                let gameType = this.getAttribute("data-type")
+                alert(`you clicked ${gameType}`);
+            }
+        })
+    }
+})
